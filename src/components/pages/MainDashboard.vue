eff702ae333a8cfa891feb28be18c0a4ad371c39
Hyejin Leem <jin2im@gmail.com>
2025-03-05 11:55:41 -0800

    first commit

---
+++ src/components/pages/MainDashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="dashboard">
+      <h2>Sales Dashboard</h2>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .dashboard {
+    padding: 20px;
+  }
+  </style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/components/pages/HomePage.vue';
+import MainDashboard from '@/components/pages/MainDashboard.vue';
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/chart', component: MainDashboard}
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
